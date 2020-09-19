f1920063dce349f7c1b39fc8c362acb6953cba44
FerOlivero <fernando.olivero@comafi.com.ar>
2020-09-19 17:02:06 -0300

    Se agrega la logica de descuento de minutos cuando es necesario.

--- tp2-ferolivero-pomodoro/Components/Timer.js
+++ tp2-ferolivero-pomodoro/Components/Timer.js
@@ -2,35 +2,70 @@ import React, { useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import Actions from './Actions';
 
+const initialMinute = 25;
+const initialSecond = 0;
+const maxSeconds = 59;
 let idInterval;
+let internalSeconds = initialSecond;
+let internalMinutes = initialMinute;
 export default function Timer() {
   
   const [active, setActive] = useState(false);
-  const [minutes, setMinutes] = useState(25);
-  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(initialMinute);
+  const [seconds, setSeconds] = useState(initialSecond);
 
   const activarTimer = () => {
     setActive(!active);
   }
 
   const reiniciarTimer = () => {
-    setMinutes(25);
-    setSeconds(0);
+    setMinutes(initialMinute); 
+    setSeconds(initialSecond);
     setActive(false);
+    internalMinutes = initialMinute;
+    internalSeconds = initialSecond;
+  }
+
+  const discountSecond = () => {
+    console.log('Tick')
+    internalSeconds--
+    setSeconds(internalSeconds);
+  }
+
+  const discountMinute = () => {
+    internalMinutes--;
+    setMinutes(internalMinutes);
+    console.log('Nuevo valor de minutos:', internalMinutes);
+    if (internalSeconds < 0){
+      internalSeconds = maxSeconds;
+      setSeconds(internalSeconds);
+    }
+  }
+
+  const triggerVibration = () => {
+    console.log('Vibrate'); 
+    clearInterval(idInterval);
   }
 
   useEffect(() => {
     if (active){
       console.log('Timer Activo');
       idInterval = setInterval(() => {
-        console.log(new Date);
+        discountSecond();
       }, 1000);
     } else {
       clearInterval(idInterval);
       console.log('Stop Timer');
     }
-    console.log(idInterval);
   }, [active])
+
+  useEffect(() => {
+    if (minutes === 0 && seconds === 0) {
+      triggerVibration();
+    } else if (minutes > 0  && seconds < 0){
+      discountMinute();
+    }
+  }, [seconds])
 
   return (
     <>
